791dc70bdd51bad5a594fc1a5be79259fd2b4712
Cristopher <crispc@Cristophers-MacBook-Pro.local>
2020-11-03 14:22:36 -0400

    fix: use a new default list for idchain

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/raid-guild/default-token-list/master/src/tokens/xdai.json'
+  'https://raw.githubusercontent.com/crisog/default-token-list/master/idchain-default-list.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
