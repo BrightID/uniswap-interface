43c859e71e3c052ff78d1f8e6d61f2155df4213c
Cristopher <ortega.cpp@gmail.com>
2020-12-04 15:13:46 -0400

    chore: update default tokenlist

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/crisog/default-token-list/master/idchain-default-list.json'
+  'https://unpkg.com/idchain-default-token-list@latest/build/uniswap-default.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
